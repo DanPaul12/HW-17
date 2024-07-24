db3ead6073b2e7e7a7407984b88985b08f18bcb7
DanPaul12 <dan.paul.schechter@gmail.com>
2024-07-24 11:05:35 -0700

    hw17

--- event_management.py
+++ event_management.py
@@ -2,3 +2,13 @@ class Event:
     def __init__(self, name, date):
         self.name = name
         self.date = date
+        self.participants = []
+
+    def add_participants(self, new_participant):
+        count = len(self.participants)
+        self.participants.append(new_participant)
+        count+=1
+
+    def count_participants(self):
+        count = len(self.participants)
+        print(count)
