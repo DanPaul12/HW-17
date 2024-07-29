213e94f6d2f3705d502363a6e67a9b49b2c91b40
DanPaul12 <dan.paul.schechter@gmail.com>
2024-07-29 13:16:07 -0700

    hw17

--- event_management.py
+++ event_management.py
@@ -10,6 +10,7 @@ class Event:
         count = len(self.participants)
         self.participants.append(new_participant)
         count+=1
+        print(self.participants, count)
 
     def count_participants(self):
         count = len(self.participants)
@@ -20,4 +21,12 @@ events = {}
 def add_event(name, date, participants):
     events[name]= Event(name, date, participants)
     print(f"Event {name} on date {date} has been added to Events with participants {participants}")
+
+def new_guest(name, guest):
+    if name in events:
+        events[name].add_participants(guest)
+
+add_event("Juice Party", "12/25/24", "Anne")
+add_event("Brownie Party", "12/26/24", "Jeff")
+new_guest("Juice Party", "Frank")
 
